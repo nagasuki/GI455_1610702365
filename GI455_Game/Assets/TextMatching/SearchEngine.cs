957cd8550d12e9b49ae844ef5fca4bc4dbe022d3
NAGASUK_LAPTOP\nail_ <tiyanon.saha@bumail.net>
2021-01-25 03:23:53 +0700

    Send Assignment Week1

---
+++ GI455_Game/Assets/TextMatching/SearchEngine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchEngine : MonoBehaviour
+{
+    public InputField findWord;
+    public Text wordData;
+    public string[] messages;
+
+    private string statName;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FindAnswer();
+        }
+    }
+
+    public void FindAnswer()
+    {
+        string outputWord = statName;
+
+        for (int i = 0; i < messages.Length; i++)
+        {
+            if (messages[i] == findWord.text)
+            {
+                outputWord = "[ " + $"<color=green>{findWord.text}</color>" + " ] " + " is found.";
+                break;
+            }
+            else
+            {
+                outputWord = "[ " + $"<color=red>{findWord.text}</color>" + " ] " + " is not found.";
+            }
+        }
+        wordData.text = outputWord;
+    }
+}
